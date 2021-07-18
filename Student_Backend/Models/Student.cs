de4d496f3d7ca5c22540e5661cfbcd3e09617a0b
ShohruhUzDev <shohruh7464784@mail.ru>
2021-07-18 10:11:53 +0500

    add datacontext

---
+++ Student_Backend/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student_Backend.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string  FirstName { get; set; }
+        public string  LastName { get; set; }
+        public string  Phone { get; set; }
+        public string  Address { get; set; }
+        public int  CoursNumber { get; set; }
+        public int? TeacherId { get; set; }
+        public virtual Teacher Teacher { get; set; }
+    }
+}
